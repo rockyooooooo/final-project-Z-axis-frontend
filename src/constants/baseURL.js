d62a70ae073a4580b5d527fce3fc1d9b89360f44
Allen Liao <namo50095@gmail.com>
2024-07-08 21:02:18 +0800

    try with prefix REACT_APP for env vars

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,7 +1,7 @@
 export const BACKEND_BASE_URL = process.env.NODE_ENV === 'production'
-  ? process.env.SERVER_HOST
+  ? process.env.REACT_APP_SERVER_HOST
   : 'http://localhost:5001'
 
 console.log('BACKEND_BASE_URL:', BACKEND_BASE_URL)
 console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
-console.log('process.env.SERVER_HOST:', process.env.SERVER_HOST)
+console.log('process.env.REACT_APP_SERVER_HOST:', process.env.REACT_APP_SERVER_HOST)
